954a80863320c12498c829b8e1c2a7f1463363aa
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-04-13 20:31:19 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,15 +18,15 @@ class ProductsController < ApplicationController
   end
 
   def create
-    Product.create(product_params)
-    redirect_to products_path
+    @post = Product.create(product_params)
+    render json: @post
   end
 
   def show
     @product = Product.find(params[:id])
     respond_to do |format|
       format.html { render :show }
-      format.json { render json: @product }
+      format.json { render json: @product, status: 201}
     end
   end
 
