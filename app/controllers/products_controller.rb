864829ca9c556210531ec5b763666d1f8b72d030
Sherry Elle <sherryelle@Sherrys-MacBook.local>
2018-06-15 17:38:33 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,8 +18,8 @@ class ProductsController < ApplicationController
   end
 
   def create
-    Product.create(product_params)
-    redirect_to products_path
+    @post = Product.create(product_params)
+    render json: @post, status: 201
   end
 
   def show
