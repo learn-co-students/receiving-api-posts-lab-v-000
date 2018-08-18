7c29119e57fd76808ad002c2a5fe1b4334bc5d70
TrevorSeitz <trevinla@gmail.com>
2018-08-18 10:34:53 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,8 +18,12 @@ class ProductsController < ApplicationController
   end
 
   def create
-    Product.create(product_params)
-    redirect_to products_path
+    @product = Product.create(product_params)
+    # respond_to do |format|
+    #   format.html { render :show }
+    #   format.json { render json: @product }
+    # end
+    render json: @product, status: 201
   end
 
   def show
