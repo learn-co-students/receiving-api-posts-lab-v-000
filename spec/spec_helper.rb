66d9c471c8ba69445d193780fc6f082f28e39c2e
github username <purnima.nyc@gmail.com>
2018-08-23 01:13:01 -0400

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,11 @@
+require 'capybara/poltergeist'
+Capybara.javascript_driver = :poltergeist
+
+options = {js_errors: false}
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, options)
+end
+
 RSpec.configure do |config|
 
   config.expect_with :rspec do |expectations|
