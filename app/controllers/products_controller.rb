9a306670cf05b738e342ddcbd6d34e0bcaca7739
FionaDL <github email address>
2019-05-22 11:25:04 +0200

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,8 +18,8 @@ class ProductsController < ApplicationController
   end
 
   def create
-    Product.create(product_params)
-    redirect_to products_path
+    @product = Product.create(product_params)
+    render json: @product, staus: 202
   end
 
   def show
